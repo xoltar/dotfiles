b655784a13a10c7c3903371214ac84f1e0c925f3
Bryn Keller <xoltar@xoltar.org>
2017-09-20 14:57:52 -0700

    Move-in script

---
+++ move-in.sh
@@ -0,0 +1,9 @@
+dotfiles=$( cd $(dirname $0) ; pwd -P )
+
+cd ~
+ln -s ${dotfiles}/.vim
+ln -s ${dotfiles}/vimrc_link.vim .vim
+ln -s ${dotfiles}/.gitconfig
+ln -s ${dotfiles}/.zshrc
+git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
+
