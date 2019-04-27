cd6e0908c1f9ed8e759d2463c55f8c778616affa
Bryn Keller <xoltar@xoltar.org>
2019-04-27 16:28:55 -0700

    updated movein

--- move-in.sh
+++ move-in.sh
@@ -6,4 +6,6 @@ ln -s ${dotfiles}/vimrc_link.vim .vim
 ln -s ${dotfiles}/.gitconfig
 ln -s ${dotfiles}/.zshrc
 git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
+git clone https://github.com/syl20bnr/spacemacs .emacs.d
+
 
