f86df4dcd887739fd3e95636df33abd640d95d06
Keller, Bryn <bryn.keller@intel.com>
2019-12-17 10:41:26 -0800

    add ideavimrc to move-in.sh

--- move-in.sh
+++ move-in.sh
@@ -3,10 +3,14 @@ dotfiles=$( cd $(dirname $0) ; pwd -P )
 cd ~
 ln -s ${dotfiles}/.vim
 ln -s ${dotfiles}/vimrc_link.vim .vim
+mkdir -p .config/nvim
+ln -s ${dotfiles}/nvim-init.vim .config/nvim/init.vim
+ln -s ${dotfiles}/.ideavimrc
 ln -s ${dotfiles}/.gitconfig
 ln -s ${dotfiles}/.zshrc
 ln -s ${dotfiles}/.zshenv
-git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
+#  git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/syl20bnr/spacemacs .emacs.d
 ln -s ${dotfiles}/.spacemacs.d
 
