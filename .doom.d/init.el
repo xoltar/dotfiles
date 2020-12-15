30a6b8cbc96a2d1bf124b743e5cba77c5083794d
Keller, Bryn <bryn.keller@intel.com>
2020-12-15 09:03:33 -0800

    lsp

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -110,7 +110,7 @@
 
        :lang
        (agda +local)              ; types of types of types of types...
-       ;;cc                ; C/C++/Obj-C madness
+       (cc +lsp)                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -149,7 +149,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +conda)            ; beautiful is better than ugly
+       (python +conda +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
